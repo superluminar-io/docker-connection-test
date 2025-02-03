971d091829af0b3dd5cb9f358c834821e7771a91
Till Kahlbrock <till.kahlbrock@gmail.com>
2025-02-03 16:35:37 +0100

    Init

---
+++ go.mod
@@ -0,0 +1,13 @@
+module example.com/docker-connection-test
+
+go 1.22.5
+
+require (
+	github.com/caarlos0/env v3.5.0+incompatible
+	github.com/go-sql-driver/mysql v1.8.1
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+
+	"database/sql"
+	"github.com/caarlos0/env"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type config struct {
+	DbHost string `env:"DB_HOST"`
+	DbUser string `env:"DB_USERNAME"`
+	DbPass string `env:"DB_PASSWORD"`
+}
+
+func main() {
+	var cfg config
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		if err != nil {
+			return
+		}
+	})
+
+	http.HandleFunc("/db", func(w http.ResponseWriter, r *http.Request) {
+		db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/", cfg.DbUser, cfg.DbPass, cfg.DbHost))
+		if err != nil {
+			panic(err.Error())
+		}
+		defer db.Close()
+
+		// Prepare statement for reading data
+		stmtOut, err := db.Prepare(`SELECT 1`)
+		if err != nil {
+			panic(err.Error())
+		}
+		defer stmtOut.Close()
+
+		_, err = fmt.Fprintf(w, "DB Connection successful!")
+		if err != nil {
+			return
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
